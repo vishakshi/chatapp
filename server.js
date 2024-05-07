683cc7d565b27814012fec6d4ca36a7ffa2ddc82
Vishakshi Gupta <162357622+vishakshi@users.noreply.github.com>
2024-05-07 12:13:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
 
-  socket.on('chat message', (msg) => {
-    console.log('message:', msg);
-    io.emit('chat message', msg);  // Broadcast to all connected clients
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected:', socket.id);
-  });
-});
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+})
